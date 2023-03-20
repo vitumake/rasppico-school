4e909b8866ef444af5d437eb378d6f6edbe46c4c
markus <mag3768@gmail.com>
2023-03-20 14:24:13 +0200

    task2 fix

--- assigment1/task2.py
+++ assigment1/task2.py
@@ -4,6 +4,7 @@ import machine, time
 led1 = machine.Pin(20, machine.Pin.OUT)
 led2 = machine.Pin(21, machine.Pin.OUT)
 led3 = machine.Pin(22, machine.Pin.OUT)
+led4 = machine.Pin('LED', machine.Pin.OUT)
 
 #Decimal to binary
 def binary(num):
@@ -22,7 +23,9 @@ for i in range(0, 8):
 
 while True:
     for b in arr:
+        led4.value(0)
         time.sleep(1)
+        led4.value(1)
         led1.value(b[0])
         led2.value(b[1])
         led3.value(b[2])
