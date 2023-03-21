8dffba7282d0eea81f5f7cc786b298872af4c6f8
markus <mag3768@gmail.com>
2023-03-21 09:35:17 +0200

    task3

--- assigment1/task3.py
+++ assigment1/task3.py
@@ -1,19 +1,34 @@
-import math, time, machine
+from machine import Pin
+from time import sleep_ms
 
-led = machine.Pin(20)
-pwmLed = machine.PWM(led)
+led = Pin(20, Pin.OUT)
+led1 = Pin(21, Pin.OUT)
+led2 = Pin(22, Pin.OUT)
 
-#pwmLed.freq(500)
-#pwmLed.duty_u16(312)
+buttonW1 = Pin(9, Pin.IN, Pin.PULL_UP)
+buttonW2 = Pin(8, Pin.IN, Pin.PULL_UP)
+buttonW3 = Pin(7, Pin.IN, Pin.PULL_UP)
+buttonRot = Pin(12, Pin.IN, Pin.PULL_UP)
 
-#Kesken!!!!
 
-def ledOn():
-    for i in range(20):
-        pwmLed.duty_u16(int((i/10)**2)*500+500)
-        time.sleep_ms(10)
-        
-def ledOff():
-    for i in range(20):
-        pwmLed.duty_u16(int(((i/10)**2)*-1)*500+500)
-        time.sleep_ms(10)
+def offAll(pin):
+    led.value(0)
+    led1.value(0)
+    led2.value(0)
+
+
+def btnHandler(btn, led):
+    while btn.value() == 0:
+        sleep_ms(1)
+        if btn.value()==1:
+            led.toggle()
+            break
+
+
+buttonRot.irq(handler=offAll, trigger=Pin.IRQ_FALLING)
+
+
+while True:
+    btnHandler(buttonW1, led)
+    btnHandler(buttonW2, led1)
+    btnHandler(buttonW3, led2)
