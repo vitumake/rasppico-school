b30a4cfe0604eb4f4dfb06eb798ee076b6d3aea3
markus <mag3768@gmail.com>
2023-04-04 08:59:30 +0300

    working ssd1036 lib

--- E2_vallin.py
+++ E2_vallin.py
@@ -1,6 +1,13 @@
 from clss import Led, Btn
 from time import sleep_ms
 from machine import I2C, Pin
+from ssd1306 import SSD1306_I2C
+
+# display
+i2c = I2C(1, sda=Pin(14), scl=Pin(15))
+disp = SSD1306_I2C(128, 64, i2c)
+
+
 
 # toggle tuple array
 toggles = [
@@ -25,5 +32,4 @@ while True:
     for i in toggles:
         if i[0].pulseCheck():
             i[1].toggle()
-            
-    
+        
