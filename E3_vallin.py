9203bd7f0239c99f89b8f33d499f6f3156cb3654
markus <mag3768@gmail.com>
2023-04-17 23:36:42 +0300

    added doc

--- E3_vallin.py
+++ E3_vallin.py
@@ -12,22 +12,28 @@ def recSampl(penis):
 tmr = Piotimer(mode=Piotimer.PERIODIC, freq=freq, callback=recSampl)
 
 def calcBPM(data):
-    rising = False
+    secRising = False
     risngFst = None
     risgnSec = None
     tresh = 1000 # Treshold for finging rising edge
-    margin = 500
+    margin = 500 # Margin when checking if value is under treshold
     
-    for i, val in enumerate(data.data):
-        if not rising:
-            if val > data.average() + tresh and risngFst == None:
+    for i, val in enumerate(data.data): # Get value and index from arr
+        # Check if we are looking for the first or second index
+        if not secRising:
+            # If first index is empty set the index
+            if val > data.average() + tresh and risngFst == None: 
                 risngFst = i
+            # Check if the value has dropped below the threshold to indicate that we can find the second value
             elif val < data.average() + tresh - margin and risngFst != None:
-                rising = True
+                secRising = True
+        # Find the second index
         elif val > data.average() + tresh:
             risgnSec = i
-    
+    # Make sure both indexes are valid
     if risngFst != None and risgnSec != None:
+        # Calculate time between indexes. Time between samples is 1/freq s.
+        # BPS is 2nd - 1st * time between samples.
         bpm = f'{(int(risgnSec) - int(risngFst)) * 1/freq * 60} BPM'
     else: bpm = 'Bad data'
     
@@ -36,5 +42,6 @@ def calcBPM(data):
 while True:
     if sampls.full:
         print(calcBPM(sampls))
+        # Clear data for new samples
         sampls.clear()
     
