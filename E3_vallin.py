2b091a4991c2777b8dd20d08235b57301a399b70
markus <mag3768@gmail.com>
2023-04-18 10:25:55 +0300

    updated to use positive peak

--- E3_vallin.py
+++ E3_vallin.py
@@ -12,36 +12,43 @@ def recSampl(penis):
 tmr = Piotimer(mode=Piotimer.PERIODIC, freq=freq, callback=recSampl)
 
 def calcBPM(data):
-    secRising = False
-    risngFst = None
-    risgnSec = None
-    tresh = 1000 # Treshold for finging rising edge
-    margin = 500 # Margin when checking if value is under treshold
+    peakFst = peakSec = secVal = None
+    maxPeak = 0
+    tresh = sorted(data.data)[round(data.size/2)] # Treshold for finding rising edge (median)
+    margin = 100 # Margin when checking if value is under treshold
     
     for i, val in enumerate(data.data): # Get value and index from arr
         # Check if we are looking for the first or second index
-        if not secRising:
+        if not secVal:
             # If first index is empty set the index
-            if val > data.average() + tresh and risngFst == None: 
-                risngFst = i
+            if val > maxPeak: 
+                peakFst = i
+                maxPeak = val
             # Check if the value has dropped below the threshold to indicate that we can find the second value
-            elif val < data.average() + tresh - margin and risngFst != None:
-                secRising = True
+            elif val < tresh - margin and maxPeak > tresh:
+                secVal = True
+                maxPeak = 0
         # Find the second index
-        elif val > data.average() + tresh:
-            risgnSec = i
+        elif val > maxPeak:
+            peakSec = i
+            maxPeak = val
     # Make sure both indexes are valid
-    if risngFst != None and risgnSec != None:
+    if peakFst != None and peakSec != None:
         # Calculate time between indexes. Time between samples is 1/freq s.
         # BPS is 2nd - 1st * time between samples.
-        bpm = f'{(int(risgnSec) - int(risngFst)) * 1/freq * 60} BPM'
-    else: bpm = 'Bad data'
+        bpm = (int(peakSec) - int(peakFst)) * 1/freq * 60
+    else: bpm = 0
     
-    return bpm 
+    return f'{bpm} BPM' if bpm < 240 and bpm > 30 else 'Bad data'#, [tresh, margin], [peakFst, peakSec]
         
 while True:
     if sampls.full:
         print(calcBPM(sampls))
+        
+        # Debug
+        #print(sampls.data)
+        #print(f'max = {max(sampls.data)} min = {min(sampls.data)}')
+        
         # Clear data for new samples
         sampls.clear()
     
